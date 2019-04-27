580606c102ad1b65b31594c63401f7f012c98dd2
Bomb-X <bombix34@gmail.com>
2019-04-28 01:20:27 +0200

    Face Random Generator ( man or woman )

---
+++ LudumDare44/Assets/Scripts/FaceDatabase.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "LD44/FaceDataBase")]
+public class FaceDatabase : ScriptableObject
+{
+    [SerializeField]
+    List<Sprite> facesMen;
+    [SerializeField]
+    List<Sprite> eyesMen;
+    [SerializeField]
+    List<Sprite> hairMen;
+  //  [SerializeField]
+  //  List<Sprite> mouthsMen;
+
+    [SerializeField]
+    List<Sprite> facesWomen;
+    [SerializeField]
+    List<Sprite> eyesWomen;
+    [SerializeField]
+    List<Sprite> hairWomen;
+
+    [SerializeField]
+    List<Sprite> mouths;
+  //  List<Sprite> mouthsWomen;
+
+    [SerializeField]
+    List<Sprite> nose;
+
+    public List<Sprite> GetCharacter(bool isMan)
+    {
+        List<Sprite> result = new List<Sprite>();
+        result.Add(mouths[(int)Random.Range(0f, mouths.Count)]);
+        // result.Add(isMan? mouthsMen[(int)Random.Range(0f,mouthsMen.Count)] : mouthsWomen[(int)Random.Range(0f, mouthsWomen.Count)]);
+        result.Add(isMan ? hairMen[(int)Random.Range(0f, hairMen.Count)] : hairWomen[(int)Random.Range(0f, hairWomen.Count)]);
+        result.Add(nose[(int)Random.Range(0f,nose.Count)]);
+        result.Add(isMan ? facesMen[(int)Random.Range(0f, facesMen.Count)] : facesWomen[(int)Random.Range(0f, facesWomen.Count)]);
+        result.Add(isMan ? eyesMen[(int)Random.Range(0f, eyesMen.Count)] : eyesWomen[(int)Random.Range(0f, eyesWomen.Count)]);
+        return result;
+    }
+
+}
---
+++ LudumDare44/Assets/Scripts/FaceManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceManager : MonoBehaviour
+{
+    [SerializeField]
+    FaceDatabase database;
+
+    [SerializeField]
+    SpriteRenderer mouth;
+    [SerializeField]
+    SpriteRenderer hair;
+    [SerializeField]
+    SpriteRenderer nose;
+    [SerializeField]
+    SpriteRenderer face;
+    [SerializeField]
+    SpriteRenderer eyes;
+
+
+    void Start()
+    {
+        InitRandomFace();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown("r"))
+        {
+            InitRandomFace();
+        }
+    }
+
+    public void InitRandomFace()
+    {
+        List<Sprite> result = database.GetCharacter(System.Convert.ToBoolean(Random.Range(0,2)));
+        mouth.sprite = result[0];
+        hair.sprite = result[1];
+        hair.color = Random.ColorHSV(0f, 1f, 0f, 1f, 0f, 1f, 1f,1F);
+        nose.sprite = result[2];
+        face.sprite = result[3];
+        eyes.sprite = result[4];
+        eyes.color = Random.ColorHSV(0f, 1f, 0f, 1f, 0f, 1f, 1f, 1F);
+    }
+}
