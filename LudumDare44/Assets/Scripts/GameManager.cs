e28213268e499f820d48b4e1c31474e79a523b38
AXG <axel.garcia@comptalia.com>
2019-04-28 18:53:56 +0200

    Ui

---
+++ LudumDare44/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    enum TurnState
+    {
+        WEDDING,
+        EVENT,
+    }
+    public int Turn { get; set; }
+    public int GoldCoins { get; set; }
+    public int InfluencePoints { get; set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
